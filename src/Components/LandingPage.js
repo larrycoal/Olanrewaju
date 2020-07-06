156528d0e5cf92ed00ec5f74da449f14396b3ea1
Olayiwola Olanrewaju <olayiwolaolanrewaju@Olayiwolas-MacBook-Pro.local>
2020-07-06 22:51:45 +0100

    new portfolio

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useRef,useState } from 'react'
 import {gsap,Power3 } from 'gsap'
 import {CSSPlugin} from 'gsap/CSSPlugin'
-gsap.registerPlugin(CSSPlugin)
+
 import Intro from './intro'
 import ChatBot from './Bot'
 
-
+gsap.registerPlugin(CSSPlugin)
 const LandingPage = () => {
     const [view, setView]=useState(false)
     let logo = useRef(null)
