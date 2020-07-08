8245353766d532cfb9cedfb7ca2a723087a73c06
Olayiwola Olanrewaju <olayiwolaolanrewaju@Olayiwolas-MacBook-Pro.local>
2020-07-08 09:53:13 +0100

    betea

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import{Switch,Route} from 'react-router-dom'
 import LandingPage from './LandingPage'
-import AboutMe from './about'
+import AboutMe from './AboutMe'
 
 import Projects from './Project'
 
