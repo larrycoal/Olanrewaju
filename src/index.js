8ded22e36d7f8730c43ee6008fb125b14df06b67
Olayiwola Olanrewaju <larry_coal@outlook.com>
2021-07-08 18:57:43 +0100

    minor changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom'
 import './Stylesheet/main.css'
 import 'milligram'
@@ -19,7 +18,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+
